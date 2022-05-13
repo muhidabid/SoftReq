9cabd5f61db4e4e82b6b1d02c8e8ef43c8f01514
Ayesha Rauf <ayshrauf@gmail.com>
2022-05-14 00:07:19 +0500

    editor design

--- 0. DEVELOPMENT/client-angular/src/app/project/editor/editor.component.ts
+++ 0. DEVELOPMENT/client-angular/src/app/project/editor/editor.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
+
+
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+  req:Requirements[] = [];
+  currentDate: Date = new Date();
 
-  ngOnInit(): void {
+  constructor() {
+    this.req = [new Requirements("System should have 2-factor authentication", "22-05-2022")]
+    console.log("constructor called")
+    console.log("date: ", this.currentDate.toDateString)
   }
 
+  ngOnInit(): void {
+    console.log("ngOnIt claled")
+  }
+
+  AddReq(text:HTMLInputElement, d:HTMLInputElement) {
+    this.req.push(new Requirements(text.value, d.value))
+  }
+
+}
+
+class Requirements{
+  constructor(public text:string, public d:string) {
+
+  }
+
+  print():void{
+    console.log("User Story: " + this.text);
+  }
 }
