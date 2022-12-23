9f6e5dbc7033b9a4f0e7eb76af7008a8a6cf7bb4
hasanriaz121 <75634716+hasanriaz121@users.noreply.github.com>
2022-12-23 13:27:34 +0500

    Chain of Loaded Binary Classifiers
    Model: Logistic Regression
    Vectorization: TF-IDF

--- Classification/src/testImplementation/binaryclassifier/BinaryClassifierLG.py
+++ Classification/src/testImplementation/binaryclassifier/BinaryClassifierLG.py
@@ -16,10 +16,13 @@ import sklearn.model_selection
 import sklearn.preprocessing as preproc
 from sklearn.feature_extraction import text as sk_text
 import pickle
+import numpy as np
 
 classnames = ["ef", "pe", "po", "re", "se", "us"]
 models = list()
 vectorizers = list()
+has_loaded_models = False
+has_loaded_vectorizers = False
 
 
 def product(arg):
@@ -33,8 +36,32 @@ def product(arg):
 def load_models():
     for classname in classnames:
         models.append(pickle.load(open(classname+"_lr.pickel", "rb")))
+    return True
 
 
 def load_vectorizers():
     for classname in classnames:
         vectorizers.append(pickle.load(open(classname+"_tfidf.pickel", "rb")))
+    return True
+
+
+def predict(text):
+    labels = list()
+    transforms = list()
+    if(has_loaded_models and has_loaded_vectorizers):
+        for vectorizer in vectorizers:
+            transforms.append(vectorizer.transform([text]))
+
+        for i in range(len(models)):
+            labels.append(models[i].predict(transforms[i]))
+
+        labels = np.concatenate(labels, axis=0)
+        print(labels)
+
+
+if __name__ == "__main__":
+    has_loaded_models = load_models()
+    has_loaded_vectorizers = load_vectorizers()
+    print(len(models))
+    print(len(vectorizers))
+    predict(input())
